c2dd469c6817b3c349712ff230a630ca6ad9a50a
Elliot Hanna <ehanna77@gmail.com>
2020-05-10 14:17:19 -0600

    Fixed animation and broke swipe gesture

--- TonganDict/ViewController.swift
+++ TonganDict/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var flashCard: UIView!
+    @IBOutlet weak var questionCard: UIView!
+    @IBOutlet weak var answerCard: UIView!
     @IBOutlet weak var outerView: UIView!
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var QA: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var answer: UILabel!
     @IBOutlet weak var result: UILabel!
+    
+    private var showingFront = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        QA.text = ""
+        question.text = "Front"
+        answer.text = "Back"
         result.text = ""
     }
     
@@ -34,20 +39,38 @@ class ViewController: UIViewController {
         changeSearchPlaceholderColor()
     }
     
+    func flipCardAnimation() {
+        UIView.transition(from: questionCard, to: answerCard, duration: 1, options: [UIView.AnimationOptions.transitionFlipFromRight, UIView.AnimationOptions.showHideTransitionViews], completion: nil)
+//        if showingFront {
+//            UIView.transition(from: questionCard, to: answerCard, duration: 1, options: UIView.AnimationOptions.transitionFlipFromRight, completion: nil)
+//        } else {
+//            UIView.transition(from: answerCard, to: questionCard, duration: 1, options: UIView.AnimationOptions.transitionFlipFromRight, completion: nil)
+//        }
+    }
+    
     
     @IBAction func flipCard(_ gesture: UITapGestureRecognizer) {
-        if flashCard.backgroundColor !== UIColor.red {
-            flashCard.backgroundColor = UIColor.red
-        } else {
-            flashCard.backgroundColor = UIColor.white
-        }
+//        if showingFront {
+//            questionCard.backgroundColor = UIColor.red
+//            answerCard.backgroundColor = UIColor.red
+//            question.text = "HAHA"
+//            answer.text = "HAHA!"
+//            showingFront = false
+//        } else {
+//            questionCard.backgroundColor = UIColor.white
+//            answerCard.backgroundColor = UIColor.white
+//            question.text = "BOO"
+//            answer.text = "BOOO!"
+//            showingFront = true
+//        }
+        flipCardAnimation()
     }
     
     @IBAction func needsReview(_ gesture: UISwipeGestureRecognizer) {
-        print("NEEDS REVIEW")
+        result.text = "Needs review"
     }
     
     @IBAction func gotItCorrect(_ gesture: UISwipeGestureRecognizer) {
-        print("GOT IT CORRECT")
+        result.text = "Got it correct"
     }
 }
